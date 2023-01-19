d7462cc6979a60b88aaf6262c831b2c28692f2a0
Chris <chris@freeperiod.co.uk>
2023-01-19 20:13:58 +0000

    yac

--- App.js
+++ App.js
@@ -1,8 +1,10 @@
 import "expo-dev-client";
 import { useEffect, useState } from "react";
-import { StatusBar } from "expo-status-bar";
-import { ScrollView, StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from 'expo-status-bar';
 import dgram from "react-native-udp";
+
+import drone from "./drone.js";
 
 function randomPort() {
   return (Math.random() * 60536) | (0 + 5000); // 60536-65536
@@ -19,70 +21,81 @@ function toByteArray(obj) {
 }
 
 export default function App() {
+
   const [chatter, setChatter] = useState([]);
 
   const updateChatter = (msg) => {
     setChatter((prev) => prev.concat([msg]));
   };
 
+  
   useEffect(() => {
+
     let a = dgram.createSocket("udp4");
     let aPort = randomPort();
     a.bind(aPort, function (err) {
       if (err) throw err;
       updateChatter("a bound to " + JSON.stringify(a.address()));
     });
-
-    let b = dgram.createSocket("udp4");
-    var bPort = randomPort();
-    b.bind(bPort, function (err) {
-      if (err) throw err;
-      updateChatter("b bound to " + JSON.stringify(b.address()));
-    });
+    
+    //let b = dgram.createSocket("udp4");
+    //var bIp = drone.ip
+    //var bPort = drone.port;
+    //b.bind(bPort, function (err) {
+    //  if (err) throw err;
+    //  updateChatter("b bound to " + JSON.stringify(b.address()));
+    //});
 
     a.on("message", function (data, rinfo) {
       var str = String.fromCharCode.apply(null, new Uint8Array(data));
       updateChatter("a received echo " + str + " " + JSON.stringify(rinfo));
       a.close();
       a = null;
-      b.close();
-      b = null;
+    //  b.close();
+    //  b = null;
     });
+const data = "Hello";
 
-    b.on("message", function (data, rinfo) {
-      var str = String.fromCharCode.apply(null, new Uint8Array(data));
-      updateChatter("b received " + str + " " + JSON.stringify(rinfo));
-
-      // echo back
-      b.send(data, 0, data.length, aPort, "127.0.0.1", function (err) {
+      a.send(data, 0, data.length, 4000, "127.0.0.1", function (err) {
         if (err) throw err;
         updateChatter("b echoed data");
       });
-    });
+    //});
 
-    b.once("listening", function () {
-      var msg = toByteArray("hello");
-      a.send(msg, 0, msg.length, bPort, "127.0.0.1", function (err) {
-        if (err) throw err;
-        updateChatter("a sent data");
-      });
-    });
+    //b.on("message", function (data, rinfo) {
+    //  var str = String.fromCharCode.apply(null, new Uint8Array(data));
+    //  updateChatter("b received " + str + " " + JSON.stringify(rinfo));
 
+      // echo back
+    //  b.send(data, 0, data.length, aPort, "127.0.0.1", function (err) {
+    //    if (err) throw err;
+    //    updateChatter("b echoed data");
+    //  });
+    //});
+
+    //b.once("listening", function () {
+    //  var msg = toByteArray("hello");
+    //  a.send(msg, 0, msg.length, bPort, "127.0.0.1", function (err) {
+    //    if (err) throw err;
+    //    updateChatter("a sent data");
+    //  });
+    //});
+    
     return () => {
       if (a) a.close();
-      if (b) b.close();
+      //if (b) b.close();
     };
+    
   }, []);
 
   return (
     <View style={styles.container}>
-      <ScrollView>
-        {chatter.map((msg, index) => (
+      <Text>{JSON.stringify(drone)}</Text>
+      {chatter.map((msg, index) => (
           <Text key={index} style={styles.welcome}>
             {msg}
           </Text>
         ))}
-      </ScrollView>
       <StatusBar style="auto" />
     </View>
   );
@@ -91,10 +104,9 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-    backgroundColor: "#f5fcff",
-    paddingTop: 20,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
   },
   welcome: {
     fontSize: 20,
---
+++ drone.js
@@ -0,0 +1,6 @@
+const drone = {
+    "ip": "192.168.10.1",
+    "port": 4000
+}
+
+export default drone;
